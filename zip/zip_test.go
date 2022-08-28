2310bc81cee7b4424d26a7382e20274007b35637
CHIKAMATSU Naohiro <n.chika156@gmail.com>
2022-08-28 22:25:28 +0900

    Remove deprecated package

--- zip/zip_test.go
+++ zip/zip_test.go
@@ -11,7 +11,6 @@ import (
 	"fmt"
 	"hash"
 	"io"
-	"io/ioutil"
 	"sort"
 	"strings"
 	"testing"
@@ -286,7 +285,7 @@ func testZip64(t testing.TB, size int64) *rleBuffer {
 			t.Fatal("read:", err)
 		}
 	}
-	gotEnd, err := ioutil.ReadAll(rc)
+	gotEnd, err := io.ReadAll(rc)
 	if err != nil {
 		t.Fatal("read end:", err)
 	}
