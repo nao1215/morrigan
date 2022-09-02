05bff5a3ed771b98276c658fbfaf0871db243d2b
CHIKAMATSU Naohiro <n.chika156@gmail.com>
2022-09-02 22:57:03 +0900

    Add unit test

--- internal/embedded/embeded_test.go
+++ internal/embedded/embeded_test.go
@@ -82,3 +82,68 @@ func TestTargetLogList(t *testing.T) {
 		})
 	}
 }
+
+func TestLicense(t *testing.T) {
+	type args struct {
+		pkg string
+	}
+	tests := []struct {
+		name    string
+		args    args
+		want    []string
+		wantErr bool
+	}{
+		{
+			name: "success",
+			args: args{
+				pkg: "morrigan",
+			},
+			want: []string{
+				"MIT License",
+				"",
+				"Copyright (c) 2022 CHIKAMATSU Naohiro",
+				"",
+				"Permission is hereby granted, free of charge, to any person obtaining a copy",
+				"of this software and associated documentation files (the \"Software\"), to deal",
+				"in the Software without restriction, including without limitation the rights",
+				"to use, copy, modify, merge, publish, distribute, sublicense, and/or sell",
+				"copies of the Software, and to permit persons to whom the Software is",
+				"furnished to do so, subject to the following conditions:",
+				"",
+				"The above copyright notice and this permission notice shall be included in all",
+				"copies or substantial portions of the Software.",
+				"",
+				"THE SOFTWARE IS PROVIDED \"AS IS\", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR",
+				"IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,",
+				"FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE",
+				"AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER",
+				"LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,",
+				"OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE",
+				"SOFTWARE.",
+				"",
+			},
+			wantErr: false,
+		},
+		{
+			name: "no-exist package does not exist",
+			args: args{
+				pkg: "no-exist",
+			},
+			want:    nil,
+			wantErr: true,
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got, err := License(tt.args.pkg)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("License() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("License() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
