098889599acc89bf4a132255e7cb5ce06701dcf5
CHIKAMATSU Naohiro <n.chika156@gmail.com>
2022-09-11 16:30:34 +0900

    Add unit test for pwlist()

---
+++ cmd/pwlist_test.go
@@ -0,0 +1,34 @@
+// Package cmd manages the entry points for the subcommands that morrigan has.
+package cmd
+
+import (
+	"os"
+	"testing"
+
+	"github.com/nao1215/morrigan/file"
+	"github.com/spf13/cobra"
+)
+
+func Test_pwlist(t *testing.T) {
+	t.Run("Success generate password list", func(t *testing.T) {
+		cmd := &cobra.Command{}
+		cmd.Flags().StringP("output", "o", "password-list", "specifies the output directory")
+		cmd.Flags().Set("output", "testdata/passwd")
+
+		if err := pwlist(cmd, []string{}); err != nil {
+			t.Errorf("failed to generate password list: %v", err)
+		}
+
+		if !file.IsFile("testdata/passwd/worst/2020-worst-200.txt") {
+			t.Errorf("failed to generate testdata/passwd/worst/2020-worst-200.txt")
+		}
+
+		if !file.IsFile("testdata/passwd/worst/worst-500.txt") {
+			t.Errorf("failed to generate testdata/passwd/worst/worst-500.txt")
+		}
+
+		if err := os.RemoveAll("./testdata/passwd"); err != nil {
+			t.Fatal(err)
+		}
+	})
+}
