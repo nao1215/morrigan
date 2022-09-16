765cb1cc34a595278bac1185e4a7f1eb9ccea1ae
CHIKAMATSU Naohiro <n.chika156@gmail.com>
2022-09-16 22:55:36 +0900

    Add unit test for print package

--- internal/print/print.go
+++ internal/print/print.go
@@ -15,23 +15,30 @@ var (
 	Stderr = colorable.NewColorableStderr()
 )
 
-// Info print information message at STDOUT.
+// Info print information message at STDOUT in green.
+// This function is used to print some information (that is not error) to the user.
 func Info(msg string) {
 	fmt.Fprintf(Stdout, "%s: %s\n", color.GreenString("INFO "), msg)
 }
 
-// Warn print warning message at STDERR.
+// Warn print warning message at STDERR in yellow.
+// This function is used to print warning message to the user.
 func Warn(err interface{}) {
 	fmt.Fprintf(Stderr, "%s: %v\n", color.YellowString("WARN "), err)
 }
 
-// Err print error message at STDERR.
+// Err print error message at STDERR in yellow.
+// This function is used to print error message to the user.
 func Err(err interface{}) {
 	fmt.Fprintf(Stderr, "%s: %v\n", color.HiYellowString("ERROR"), err)
 }
 
-// Fatal print dying message at STDERR.
+// OsExit is wrapper for  os.Exit(). It's for unit test.
+var OsExit = os.Exit
+
+// Fatal print dying message at STDERR in red.
+// After print message, process will exit
 func Fatal(err interface{}) {
 	fmt.Fprintf(Stderr, "%s: %v\n", color.RedString("FATAL"), err)
-	os.Exit(1)
+	OsExit(1)
 }
